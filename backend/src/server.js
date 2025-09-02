7216ed9e7c84b327341ca411354633bf5af2c1f5
Giv <aminsarlak1925@gmail.com>
2025-09-02 12:54:48 +0300

    FIxed pathes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
@@ -16,7 +14,7 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5004;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
@@ -32,10 +30,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
   });
 }
 
